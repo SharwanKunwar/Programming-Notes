d244c6a6e134ca145fb100c91cc94dbc183f0561
SharwanKunwar <sharwankunwar07@gmail.com>
2025-05-31 19:27:41 +0545

    added animation on text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import HomeSecondSection from "./Home/HomeSecondSection";
+import {motion} from 'motion/react';
 
 
 function Home() {
@@ -20,14 +21,46 @@ function Home() {
         <div style={style01} className=" flex justify-center items-center md:w-full md:h-full w-full h-6/12">
           <section className=" w-full md:h-full flex flex-col md:justify-end justify-center pl-5 pr-5 ">
           <div className="text-center z-10 md:bg-white h-[250px] md:pt-7">
-            <h1 className="text-4xl font-bold mb-2 ">Download Programming Notes Easily</h1>
-            <h3 className="text-2xl mb-5">Programming, Frameworks, Libraries, Databases, AWS etc.</h3>
-            <p className="text-gray-700">
+            <motion.h1
+            initial={{ y: -50, opacity: 0, filter: 'blur(5px)' }}
+            animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+            transition={{
+              duration: 1,
+              delay:0.1,
+              ease: 'easeOut',
+            }}
+            className="text-4xl font-bold mb-2 ">Download Programming Notes Easily</motion.h1>
+            <motion.h3
+            initial={{ y: -50, opacity: 0, filter: 'blur(5px)' }}
+            animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+            transition={{
+              duration: 1,
+              delay:0.2,
+              ease: 'easeOut',
+            }}
+            className="text-2xl mb-5">Programming, Frameworks, Libraries, Databases, AWS etc.</motion.h3>
+            <motion.p
+            initial={{ y: -50, opacity: 0, filter: 'blur(5px)' }}
+            animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+            transition={{
+              duration: 1,
+              delay:0.3,
+              ease: 'easeOut',
+            }}
+            className="text-gray-700">
             Get concise, well-organized notes on various programming topics to speed up your learning.
-            </p>
-            <button className="bg-blue-600 text-white md:px-10 px-4 mt-7 py-2 rounded hover:bg-blue-700">
+            </motion.p>
+            <motion.button 
+            initial={{ y: -50, opacity: 0, filter: 'blur(5px)' }}
+            animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+            transition={{
+              duration: 1,
+              delay:0.4,
+              ease: 'easeOut',
+            }}
+            className="bg-blue-600 text-white md:px-10 px-4 mt-7 py-2 rounded hover:bg-blue-700">
               Explore Notes
-            </button>
+            </motion.button>
           </div>
           </section>
         </div>
