59d8a922d1c5bb68beb7fbd539683415955a26f0
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-02 17:18:32 +0545

    updated with pds and download feature

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,17 @@
 
 import React from 'react'
 
-function Card({title,des}) {
+function Card({title,des,id,view}) {
+
+const handleClick = (e) => {
+    if (e.target.id === id && view) {
+      window.open(view, '_blank');
+    }
+  };
+
+
+
+
   return (
     <div className='bg-gray-50 w-[400px] md:w-[300px] h-[400px] rounded-2xl flex justify-center items-center flex-col shadow-lg'>
       <div className=' w-full h-6/17 flex justify-center items-center'>
@@ -9,9 +19,9 @@ function Card({title,des}) {
       </div>
       <div className=' w-full h-6/12'>
       <h1 className='text-center text-2xl font-bold'>{title}</h1>
-      <p className='pt-5 pb-5 pr-2 pl-5 text-black'>{des}</p>
+      <p className='pt-5 pb-5 md:pl-4 md:pr-1 px-5 text-black'>{des}</p>
       <section className='w-full flex justify-center mt-3'>
-        <button className='bg-sky-400 text-white font-medium w-[80%] h-[45px] rounded-lg'>Download Now</button>
+        <button onClick={handleClick} id={id} className='bg-sky-400 text-white font-medium w-[80%] h-[45px] rounded-lg'>Download Now</button>
       </section>
       </div>
     </div>
--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -5,8 +5,8 @@ function Notes() {
   return (
     <div className='w-screen '>
       <div className='flex flex-wrap gap-10 md:pl-30 p-9'>
-      <Card title={"C"} des={"C is a fast, low-level, procedural language developed by Dennis Ritchie in the 1970s for system programming."}/>
-      <Card title={"JAVA"} des={"Java is an OOP language by Sun (1995), platform-independent via JVM: write once, run anywhere."}/>
+      <Card view ="/NotesPdfs/BIG.pdf" title={"C"} des={"C is a fast, low-level, procedural language developed by Dennis Ritchie in the 1970s for system programming."} id={"c"}/>
+      <Card title={"JAVA"} des={"Java is an OOP language by Sun (1995), platform-independent via JVM: write once, run anywhere."} id={"java"}/>
       <Card/>
       <Card/>
       <Card/>
