6a063c33a98da84dc6bf30d8dd82ff480eb66a16
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-02 19:53:05 +0545

    changes in links

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -9,7 +9,7 @@ function SocialLinks() {
               <section className="w-full h-[100px] flex items-center justify-center gap-10 md:hidden">
                   <a href="#facebook"><FaFacebook size={35} className="text-blue-600 cursor-pointer hover:text-blue-400 hover:border-b-2" /></a>
                   <a href="#instagram"><FaInstagram size={35} className="text-pink-500 cursor-pointer hover:text-pink-400 hover:border-b-2" /></a>
-                  <a href="#github"><FaGithub size={35} className="text-blue-600 cursor-pointer hover:text-blue-400 hover:border-b-2" /></a>
+                  <a href="https://github.com/SharwanKunwar"><FaGithub size={35} className="text-blue-600 cursor-pointer hover:text-blue-400 hover:border-b-2" /></a>
               </section>
     </div>
   )
